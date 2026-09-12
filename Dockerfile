3e1fc0eeb53e3da197162df3da7b41cd3cf15722
tangky125 <117249191+tangky125@users.noreply.github.com>
2026-09-12 18:36:33 +1000

    Task 4.3

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM python:3.12-slim
+FROM python:3.12-slim AS base
 
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 5000
+RUN useradd -m appuser && chown -R appuser /app
+USER appuser
 
+EXPOSE 8080
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+visit_count = 0
 
 @app.route('/')
 def home():
-    return 'Hello from my containerized Flask app!'
+    global visit_count
+    visit_count += 1
+    return render_template('index.html', time=datetime.now(), visits=visit_count)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
